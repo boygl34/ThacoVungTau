cb92d63bc729cf25d85652bdc9f7ed20608cd2f8
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-11-01 20:55:10 +0700

    add
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- js/Login.js
+++ js/Login.js
@@ -1,23 +1,25 @@
-function DangNhap(){
-    var urluser = "https://big-road-newsstand.glitch.me/User/"
-    var password = $("#password_use").val()
-    fetch(urluser+$("#USER").val())
+function DangNhap() {
+  var urluser = "https://big-road-newsstand.glitch.me/User/"
+  var password = $("#password_use").val()
+  fetch(urluser + $("#USER").val())
     .then(response => response.json())
-    .then(data =>{  
-      if(password==data.Password){
-       // checkPhanQuyen()
-          localStorage.setItem("userName",data.useName)
-          localStorage.setItem("Password",data.Password)
-          localStorage.setItem("id",data.id)
-          localStorage.setItem("Ten",data.Ten)
-          localStorage.setItem("PhanQuyen",data.PhanQuyen)
-          localStorage.setItem("BoPhan",data.BoPhan)
-          window.location="index.html";
-        }
-       else{
-          alert("Sai pass")}
+    .then(data => {
+      if (password == data.Password) {
+        // checkPhanQuyen()
+        localStorage.setItem("userName", data.useName)
+        localStorage.setItem("Password", data.Password)
+        localStorage.setItem("id", data.id)
+        localStorage.setItem("Ten", data.Ten)
+        localStorage.setItem("PhanQuyen", data.PhanQuyen)
+        localStorage.setItem("BoPhan", data.BoPhan)
+        localStorage.setItem("TabDaDong", "html/Home.html");
+        window.location = "index.html";
+      }
+      else {
+        alert("Sai pass")
+      }
     })
     .catch((error) => {
       alert(error)
-      });
-    }
+    });
+}

4a5d87c3a052fa86dc26896539cdd0caf4314325
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-11-18 09:32:21 +0700

    add
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- js/Background.js
+++ js/Background.js
@@ -122,27 +122,50 @@ checkPhanQuyen();
 function checkPhanQuyen() {
   var PhanQuyen = localStorage.getItem("PhanQuyen");
   var BoPhan = localStorage.getItem("BoPhan")
-  console.log(BoPhan)
-  if (PhanQuyen == "admin") { document.getElementById("Setting").style.display = "block" } else {
-    document.getElementById("Setting").style.display = "none"
+  if (PhanQuyen == "admin" || PhanQuyen == "LeTan") {
+    $("#fonmid").show();
   }
-  if (BoPhan == "admin") {
-    document.getElementById("PQDichVu2").style.display = "block"
-    document.getElementById("PQPhuKien").style.display = "block"
-    document.getElementById("PQKinhDoanh").style.display = "block"
-    document.getElementById("PQManHinh").style.display = "block"
+  if (PhanQuyen == "admin") {
+    $("#Setting").show()
+
+  } else {
+    $("#Setting").hide()
+
+  }
+  if (BoPhan == "admin" || PhanQuyen == "admin") {
+    $("#PQDichVu2").show()
+    $("#PQPhuKien").show()
+    $("#PQKinhDoanh").show()
+
+    $("#PQCoVan").show()
+
   }
   if (BoPhan == "Dịch Vụ") {
-    document.getElementById("PQDichVu2").style.display = "block"
+    $("#PQDichVu2").show()
+
+
+    if (PhanQuyen == "CoVandichvu" || PhanQuyen == "admin") {
+      $("#PQCoVan").show()
+
+    }
   }
   if (BoPhan == "Phụ Kien") {
-    document.getElementById("PQPhuKien").style.display = "block"
+    $("#PQPhuKien").show()
+
   }
   if (BoPhan == "Kinh Doanh") {
-    document.getElementById("PQKinhDoanh").style.display = "block"
+    $("#PQKinhDoanh").show()
+
   }
 
+} if (BoPhan == "Phụ Kien") {
+  document.getElementById("PQPhuKien").style.display = "block"
 }
+if (BoPhan == "Kinh Doanh") {
+  document.getElementById("PQKinhDoanh").style.display = "block"
+}
+
+
 
 function Doingay(use) {
   var ThoiGian = "Chưa XN";
